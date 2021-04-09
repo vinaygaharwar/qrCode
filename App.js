40c37051f1bec6ef7900d5ccfe050c9912c14027
vinaygaharwar <vinayygaharwar@gmail.com>
2021-04-09 19:38:49 +0530

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,84 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {Text,View, ActivityIndicator} from 'react-native';
+import * as Permissions from 'expo-permissions';
+import { BarCodeScanner } from 'expo-barcode-scanner';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+class App extends React.Component{
+  static navigationOptions = {
+    header: null
+  }
+  state = {
+    hasCameraPermission: null, 
+    isScanned: false 
+  }
+  async componentDidMount() {
+    
+    const { status } = await Permissions.askAsync(Permissions.CAMERA);
+    console.log(status);
+    this.setState({ hasCameraPermission: status === "granted" ? true : false });
+  }
+
+
+
+  handleBarCodeScanned = ({ data}) => {
+      
+      <View>
+      <Text>{data}</Text>
     </View>
-  );
+  }
+  render(){
+    const { hasCameraPermission, isScanned } = this.state;
+    if(hasCameraPermission === null){
+      console.log("Requesting permission");
+      return (
+        <ActivityIndicator
+            color= "blue"
+            size= "large"
+        />
+      );
+    }
+
+    if(hasCameraPermission === false){
+      return ( 
+        <View>
+         <Text>Please grant Camera permission</Text>
+        </View> 
+      )
+    }
+    if(hasCameraPermission === true && !isScanned ){
+      return <View style = {{
+        flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center'
+
+      }}>
+        <Text>Scan code inside window</Text>
+        <BarCodeScanner
+          onBarCodeScanned = { isScanned ? undefined : this.handleBarCodeScanned }
+          style = {{
+            height:  300,
+            width: 400,
+          }}
+        >
+        </BarCodeScanner>
+      </View>
+    }
+    else{
+      return <ActivityIndicator
+      color= "blue"
+      size= "large"
+  />;
+    }
+
+
+  }
+
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+
+
+export default App;
+
+
